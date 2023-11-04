7ec46f4ce4fde33950a3307fe3a6041182e98caf
B1ack0ut1 <42845068+B1ack0ut1@users.noreply.github.com>
2023-11-04 23:42:45 +0800

    Used/modified glob expression to specify files to search for use of tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./build/*.html", "./build/js/*.js"],
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       screens: {
